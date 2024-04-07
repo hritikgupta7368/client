7bead601895b6c64db2f5b36b69beedb87ded90e
hritik <hritikgupta8637@gmail.com>
2024-04-07 13:29:59 +0530

    fix

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -5,8 +5,8 @@ const Card = () => {
   return (
     <div className="card">
       <p className="text-3xl font-semibold mb-2 md:mb-0">Original and Quality</p>
-      <p className="text-5xl font-bold mb-5 md:mb-0">Industrial and Electrical goods</p>
-      <p className='text-lg md:text-2xl font-medium mb-6 md:mb-5'>Deals in Pipes, Boiler Fittings ,Bearings, Industrial Oils and electrical components   </p>
+      <p className="heading md:text-5xl font-bold mb-5 md:mb-0">Industrial and Electrical goods</p>
+      <p className='text-lg md:text-2xl font-medium mb-10 md:mb-5'>Deals in Pipes, Boiler Fittings ,Bearings, Industrial Oils and electrical components   </p>
       <a className = "shop_button" href = "#products" >Products</a>
     </div>
   )
@@ -16,7 +16,7 @@ const Navbar = () => {
     <container id = "home" className = "h-screen flex flex-col">
 
     <nav className="nav">
-      <div className="inline-block pt-5 md:pt-0">
+      <div className="inline-block pt-3 md:pt-0">
         <Image src="/logo.jpg" height={100} width={100} alt="logo" className="inline-block mr-4"/>
         <span className="logoHeading">Megha Enterprises</span>
       </div>
